04ba13274cade5b0c1145773e59cf0e3fe47221a
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2023-04-01 11:42:59 +0530

    2nd

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  Base :"/First-Vite-React"
+  base :"/First-Vite-React"
 })
